e6c358840db89c9a0dc9e99a1fef934a72b97f74
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2011-12-11 06:28:30 +0900

    add .emacs.d

---
+++ .emacs.d/elpa/findr-0.7/findr-autoloads.el
@@ -0,0 +1,18 @@
+;;; findr-autoloads.el --- automatically extracted autoloads
+;;
+;;; Code:
+
+
+;;;### (autoloads nil nil ("findr-pkg.el" "findr.el") (19905 34048
+;;;;;;  510422))
+
+;;;***
+
+(provide 'findr-autoloads)
+;; Local Variables:
+;; version-control: never
+;; no-byte-compile: t
+;; no-update-autoloads: t
+;; coding: utf-8
+;; End:
+;;; findr-autoloads.el ends here
