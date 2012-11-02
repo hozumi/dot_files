5d290c07894919e0ede1280f0474c018ac52c300
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2012-11-02 20:42:21 +0900

    add lein profile

---
+++ .lein/profiles.clj
@@ -0,0 +1,8 @@
+{:user {:plugins [[lein-swank "1.4.4"]
+                  [lein-clojars "0.9.0"]
+                  [lein-midje "2.0.0-SNAPSHOT"]
+                  [hiccup-bridge "1.0.0-SNAPSHOT"]
+                  [lein-cljsbuild "0.2.5"]]}
+ :dev {:dependencies [[midje "1.4.0"]
+                      [com.stuartsierra/lazytest "1.2.3"]]
+       :repositories {"stuart" "http://stuartsierra.com/maven2"}}}
