471d5f37cb1c2c5dbb1e5b7be429d1cff7341f1c
Takahiro Hozumi <fatrow+hozumi@googlemail.com>
2013-04-08 13:08:59 +0900

    update midje, lein-clojars

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -1,8 +1,8 @@
 {:user {:plugins [[lein-swank "1.4.4"]
-                  [lein-clojars "0.9.0"]
-                  [lein-midje "2.0.0-SNAPSHOT"]
+                  [lein-clojars "0.9.1"]
+                  [lein-midje "3.0.0"]
                   [hiccup-bridge "1.0.0-SNAPSHOT"]
                   [lein-cljsbuild "0.2.5"]]}
- :dev {:dependencies [[midje "1.4.0"]
+ :dev {:dependencies [[midje "1.5.1"]
                       [com.stuartsierra/lazytest "1.2.3"]]
        :repositories {"stuart" "http://stuartsierra.com/maven2"}}}
